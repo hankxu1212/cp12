02f325ff6825281a993e2f6c4ea4ce1828d0a030
hankxu1212 <53340720+hankxu1212@users.noreply.github.com>
2020-04-23 09:07:10 -0700

    Update and rename ai to ai.py

---
+++ ai.py
@@ -0,0 +1,12 @@
+data augmentation
+
+datagen = ImageDataGenerator(
+rotation_range=40,
+width_shift_range=0.2,
+height_shift_range=0.2,
+shear_range=0.2,
+zoom_range=0.2,
+horizontal_flip=True,
+fill_mode='nearest')
+
+from keras import
