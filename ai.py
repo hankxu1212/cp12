d8dd85e1a7fe2c14c39d616b3df8410ec9a0a1bb
hankxu1212 <53340720+hankxu1212@users.noreply.github.com>
2020-04-23 10:25:35 -0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,12 +1,33 @@
+"""
 data augmentation
+"""
+import matplotlib.pyplot as plt
 
 datagen = ImageDataGenerator(
-rotation_range=40,
-width_shift_range=0.2,
-height_shift_range=0.2,
-shear_range=0.2,
-zoom_range=0.2,
-horizontal_flip=True,
-fill_mode='nearest')
+  rotation_range=40,
+  width_shift_range=0.2,
+  height_shift_range=0.2,
+  shear_range=0.2,
+  zoom_range=0.2,
+  horizontal_flip=True,
+  fill_mode='nearest')
 
-from keras import
+from keras.processing import image
+fnames = [os.join(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
+
+img_path = fnames[3]
+
+img = image.load_img(img_path, target_size=(150,150))
+
+x = image.img_to_array(img)
+x = x.reshape((1,) + x.shape)
+
+i = 0
+for batch in datagen.flow(x, batch_size=1):
+  plt.figure(i)
+  imgplot = plt.imshow(image.array_to_img(batch[0]))
+  i += 1
+  if i % 4 == 0:
+    break
+    
+plt.show()
